d99e2c59cfb70f6fa77af5df1019ba25fe5956fb
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-03-18 08:47:55 +0530

    timeline updated

--- src/components/Timeline/timeline.jsx
+++ src/components/Timeline/timeline.jsx
@@ -1,59 +1,56 @@
 import React from 'react';
 import VerticalTimeline from './verticalTimeline';
 import VerticalTimelineElement from './verticalTimelineElement';
-import './Timeline.css'; // Make sure to import the CSS
-
-import { Link } from 'react-router-dom';
+import './Timeline.css'; 
 
 
 const Timeline = () => {
   return (
     <div>
       <div className='w-full items-center flex justify-center mt-10'>
-      <h2 className='headingOfExp' style={{color:'white',fontSize:'30px'}}>Experience</h2>
+        <h2 className='headingOfExp' style={{ color: 'white', fontSize: '30px' }}>Experience</h2>
       </div>
       <VerticalTimeline layout="2-columns" animate={true} className="timeline-container">
         <VerticalTimelineElement
-          date="30 September 2024"
+          date="11 March 2025"
           icon={<span className="timeline-icon"><img src='#' alt='' className='h-10 w-10'></img></span>}
           position="left"
         >
-          <h3 className='underline font-semibold'>• Quiz(Python & Aptitude)</h3>
-          <p>Test your Python programming skills in our exciting Python Quiz organized by the tech society!"</p>
+          <h3 className='underline font-semibold'> Full-Stack Intern at BLU-Parrot</h3>
+          <p>Customized software solutions and contributed to international projects, ensuring scalable and efficient development.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          date="20 October 2024"
-          icon={<span className="timeline-icon">🕵️‍♂️</span>}
+          date="1 March 2025"
+          icon={<span className="timeline-icon"></span>}
           position="right"
         >
           <h3 className='underline font-semibold'>• Full Stack Developer at Ambuvians</h3>
-          <p>"Showcase your debugging skills and solve challenging code errors in our thrilling Codebugger event!"</p>
+          <p>Developed full-stack applications using React with TypeScript, integrated Kafka for high-throughput messaging, and worked with relational and non-relational databases using Prisma.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          date="20 November 2024"
-          icon={<span className="timeline-icon">💃</span>}
+          date=" November 2024"
+          icon={<span className="timeline-icon"></span>}
           position="left"
         >
-          <h3 className='underline font-semibold'>•  Full Stack Developer at Shailja Trust</h3>
-          <p>"Join us for Technocratos, an electrifying event filled with dancing, singing, and an array of exciting performances!"</p>
+          <h3 className='underline font-semibold'>• Full Stack Developer at Shailja Trust</h3>
+          <p>Designed and developed the complete website for Shailja Trust using React for the frontend and AppWrite as the backend service.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          date="16 December 2024"
-          icon={<span className="timeline-icon">📲</span>}
+          date="april 2024"
+          icon={<span className="timeline-icon"></span>}
           position="right"
         >
-          <h3 className='underline font-semibold'>•  Full Stack Developer Intern at FTV</h3>
-          <p>"Unleash your creativity with stunning visuals at Framefiesta, the ultimate multimedia presentation competition!"</p>
+          <h3 className='underline font-semibold'>• Full Stack Developer Intern at FTV</h3>
+          <p>Optimized REST API performance and enhanced system efficiency using the MERN stack.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          date="19 January 2025"
-          icon={<span className="timeline-icon">🎙️</span>}
+          date="july 2024"
+          icon={<span className="timeline-icon"></span>}
           position="left"
         >
           <h3 className='underline font-semibold'>• Joint Secretary At CSSE</h3>
-          <p>"Gain insights from industry leaders at our Expert Talk event, hosted by us"</p>
+          <p>Led various technical and non-technical events, including hackathons, ideathons, and coding competitions, fostering innovation and collaboration.</p>
         </VerticalTimelineElement>
-       
       </VerticalTimeline>
     </div>
   );
